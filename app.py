3602685272d670c3e3b4f5b6ce578d0d20d7890f
alvin-agustio <alvinagustio79@gmail.com>
2024-12-18 23:59:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
+from rembg import remove
+import io
 
 # Muat model yang sudah dilatih
 model = load_model('asl_model.h5')  # Sesuaikan dengan nama file model Anda
@@ -20,15 +22,26 @@ st.write("Upload an image of an ASL sign and get the predicted letter!")
 # Membuat widget untuk upload gambar
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
+# Fungsi untuk menghapus latar belakang gambar
+def remove_background(image_data):
+    input_image = Image.open(io.BytesIO(image_data))
+    output_image = remove(input_image)  # Proses penghapusan latar belakang
+    output_pil_image = Image.open(io.BytesIO(output_image))
+    return output_pil_image
+
 # Jika ada gambar yang diunggah, tampilkan dan prediksi
 if uploaded_file is not None:
     # Baca gambar dan tampilkan
     img = Image.open(uploaded_file)
-    st.image(img, caption='Uploaded Image', use_container_width=True)
+    st.image(img, caption='Uploaded Image', use_column_width=True)
 
-    # Preprocessing gambar yang diunggah
-    img = img.resize((128, 128))  # Sesuaikan dengan ukuran yang digunakan model Anda
-    img_array = np.array(img)
+    # Menghapus latar belakang gambar
+    img_without_bg = remove_background(uploaded_file.read())
+    st.image(img_without_bg, caption='Image without Background', use_column_width=True)
+
+    # Preprocessing gambar yang diunggah tanpa latar belakang
+    img_without_bg = img_without_bg.resize((128, 128))  # Sesuaikan dengan ukuran yang digunakan model Anda
+    img_array = np.array(img_without_bg)
     img_array = img_array / 255.0  # Normalisasi
     img_array = np.expand_dims(img_array, axis=0)  # Tambahkan dimensi batch
 
