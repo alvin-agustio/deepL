b1415c1836bf0d090cfc85bc432e9e5e35289f5c
alvin-agustio <alvinagustio79@gmail.com>
2024-12-20 11:26:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
 
-# Muat model yang sudah dilatih
-model = load_model('asl_model.h5')  # Sesuaikan dengan nama file model Anda
+# Fungsi untuk memuat model TFLite
+def load_tflite_model(model_path):
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
+# Fungsi untuk melakukan prediksi menggunakan TFLite model
+def predict_with_tflite(interpreter, img_array):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Preprocessing gambar
+    input_data = np.expand_dims(img_array, axis=0).astype(np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+
+    # Ambil hasil prediksi
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data
+
+# Muat model TFLite
+interpreter = load_tflite_model('asl_model.tflite')
 
 # Definisikan kelas yang digunakan dalam model (misalnya, huruf ASL)
 class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -20,7 +38,6 @@ st.write("Upload an image of an ASL sign and get the predicted letter!")
 # Membuat widget untuk upload gambar
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-# Jika ada gambar yang diunggah, tampilkan dan prediksi
 if uploaded_file is not None:
     # Baca gambar dan tampilkan
     img = Image.open(uploaded_file)
@@ -30,11 +47,10 @@ if uploaded_file is not None:
     img = img.resize((128, 128))  # Sesuaikan dengan ukuran yang digunakan model Anda
     img_array = np.array(img)
     img_array = img_array / 255.0  # Normalisasi
-    img_array = np.expand_dims(img_array, axis=0)  # Tambahkan dimensi batch
 
-    # Prediksi menggunakan model
-    prediction = model.predict(img_array)
-    predicted_class = np.argmax(prediction, axis=1)
+    # Prediksi menggunakan model TFLite
+    predictions = predict_with_tflite(interpreter, img_array)
+    predicted_class = np.argmax(predictions, axis=1)
 
     # Menampilkan hasil prediksi
     predicted_letter = class_names[predicted_class[0]]
